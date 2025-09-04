adc2bc06f271d0ff0c0c07f3b57968d6f067e505
LuigiCasale95 <luigicasale95@gmail.com>
2025-09-04 15:32:50 +0200

    Struttura basa dell'esercizio

---
+++ script.js
@@ -0,0 +1,30 @@
+/* Consegna
+
+Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
+Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato) */
+
+/* Selezione delle Variabili */
+
+const lampAccesa = document.querySelector(".accesa");
+console.log(lampAccesa);
+const lampSpenta = document.querySelector(".spenta");
+console.log(lampSpenta);
+const bottone = document.getElementById("bottone");
+console.log(bottone);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* FUNZIONI */
